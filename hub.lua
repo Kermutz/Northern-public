7497f64fa7889297850ec1f1e0692d1b631e4a11
1Ronkkeli <joshheard565@gmail.com>
2022-07-09 09:47:54 +0300

    as

---
+++ hub.lua
@@ -0,0 +1,5 @@
+--Arsenal Undetected
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+if game.PlaceId == 286090429 then
+
+end
