c2e35229755ba2d1a1883c62fe26072344e44f88
1Ronkkeli <joshheard565@gmail.com>
2022-07-09 09:49:34 +0300

    asd

--- hub.lua
+++ hub.lua
@@ -2,4 +2,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 if game.PlaceId == 286090429 then
 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kermutz/Northern-public/main/games/arsenal.lua"))()
+
 end
